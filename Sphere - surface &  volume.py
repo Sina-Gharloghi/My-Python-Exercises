09c8bc380987c85c402f565bdd89d32512ae536e
SINA <82410948+Sina-Gharloghi@users.noreply.github.com>
2021-11-15 14:45:08 +0330

    Add files via upload

---
+++ Sphere - surface &  volume.py
@@ -0,0 +1,12 @@
+p = 3.14
+
+r = float(input("Enter the radius : "))
+
+s = 4*p*(r**2)
+v = (4/3) * p * (r**3)
+
+print("Surface = ", s)
+print("Volume = ", v, "\n")
+
+y = input("press any key to exit ... ")
+
