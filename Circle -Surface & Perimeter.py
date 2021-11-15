d891653e9389f508de4e0fdeee13f6bd60959016
SINA <82410948+Sina-Gharloghi@users.noreply.github.com>
2021-11-15 14:08:55 +0330

    Add files via upload

---
+++ Circle -Surface & Perimeter.py
@@ -0,0 +1,10 @@
+x = 3.14
+
+r = float(input("Enter the radious  : "))
+p = 2*x*r
+s = x * (r*r)
+
+print("Surface = " , s)
+print("Primeter = " , p , "\n")
+
+y = input("press any key to exit ... ")
